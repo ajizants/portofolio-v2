53a714baca7b473a0fc2111b38bfc67b3edae6de
Aksa_Kelana <46523947+ajizants@users.noreply.github.com>
2025-12-28 13:44:56 +0700

    initial commit

---
+++ resources/js/Pages/Profile/Edit.tsx
@@ -0,0 +1,51 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { PageProps } from "@/types";
+import { Head } from "@inertiajs/react";
+import DeleteUserForm from "./Partials/DeleteUserForm";
+import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
+import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
+import { Card } from "@/components/ui/card";
+
+export default function Edit({
+    mustVerifyEmail,
+    status,
+}: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Profile
+                </h2>
+            }
+        >
+            <Head title="Profile" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <Card>
+                        <UpdateProfileInformationForm
+                            mustVerifyEmail={mustVerifyEmail}
+                            status={status}
+                            className="max-w-xl"
+                        />
+                    </Card>
+                    <Card>
+                        <UpdateProfileInformationForm
+                            mustVerifyEmail={mustVerifyEmail}
+                            status={status}
+                            className="max-w-xl"
+                        />
+                    </Card>
+
+                    <Card>
+                        <UpdatePasswordForm className="max-w-xl" />
+                    </Card>
+
+                    <Card>
+                        <DeleteUserForm className="max-w-xl" />
+                    </Card>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
---
+++ resources/js/components/mode-toggle.tsx
@@ -0,0 +1,55 @@
+import { Moon, Sun, Laptop } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/components/ThemeProvider";
+
+export function ModeToggle() {
+    const { theme, setTheme } = useTheme();
+
+    const changeTheme = () => {
+        if (theme === "light") {
+            setTheme("dark");
+        } else if (theme === "dark") {
+            setTheme("system");
+        } else {
+            setTheme("light");
+        }
+    };
+
+    return (
+        <Button
+            className="border-0"
+            variant="outline"
+            size="icon"
+            onClick={changeTheme}
+        >
+            {/* LIGHT */}
+            <Sun
+                className={`h-[1.2rem] w-[1.2rem] transition-all ${
+                    theme === "light"
+                        ? "scale-100 rotate-0"
+                        : "scale-0 rotate-90"
+                }`}
+            />
+
+            {/* DARK */}
+            <Moon
+                className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+                    theme === "dark"
+                        ? "scale-100 rotate-0"
+                        : "scale-0 rotate-90"
+                }`}
+            />
+
+            {/* SYSTEM */}
+            <Laptop
+                className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+                    theme === "system"
+                        ? "scale-100 rotate-0"
+                        : "scale-0 rotate-90"
+                }`}
+            />
+
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    );
+}
